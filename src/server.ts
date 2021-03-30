066aa5264d3b956a78519a074b09d94861a60bc9
gemma-Kim <gemmak.rok@gmail.com>
2021-03-31 02:37:07 +0900

    Modified: part of dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,14 @@
 import app from './app';
 import * as dotenv from 'dotenv';
-import { sequelize } from './models'
+import * as http from 'http';
 
 dotenv.config();
 
 const prod: boolean = process.env.NODE_ENV === 'production';
 const port = prod ? process.env.PORT : 3065
+const server = http.createServer(app)
 
-// database connection
-sequelize.sync({ force: false })
-  .then((): void => {
-    console.log('Success to connect DB');
-  })
-  .catch((err: Error): void => {
-    console.error(err);
-  })
-
-app.listen(port, (): void => {
+server.listen(port, (): void => {
   console.log(`Server is running on PORT ${port}`);
 })
 
