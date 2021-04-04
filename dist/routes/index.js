b45de39b6c7eef29c75836659da75a31098ce914
gemma-Kim <gemmak.rok@gmail.com>
2021-04-04 13:10:14 +0900

    Fixed: ts config to ignore dist directory

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var user_1 = require("./user");
-var todo_1 = require("./todo");
-var router = express_1.Router();
-router.use('/users', user_1["default"]);
-router.use('/todos', todo_1["default"]);
-exports["default"] = router;
