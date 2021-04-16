71378fbcbbe17c8111d5440be31d4659bcff16ff
Gemma-Kim <gemmak.rok@gmail.com>
2021-04-16 19:23:11 +0900

    Add Again: add post api

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,9 +1,34 @@
 import * as express from 'express'
+import { Router, Request, Response, NextFunction } from 'express';
+import { userModel, todoModel } from '../models';
+import { isLoggedIn } from './middleware';
 
 const todoRouter = express.Router();
 
-todoRouter.get('', (req, res) => {
-
+// add todo list
+todoRouter.post('', isLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // need 'list_id', 'content' in req.body
+      if (req.body.list_id && req.body.content) {
+        if (req.user) {
+          const data = {...req.body, user_id: req.user.id}
+          const newTodo = await todoModel.addTodo(data)
+          if (newTodo) {
+            return res.status(200).json(newTodo)
+          } else {
+            return res.status(400).json({ 'message': 'no created new todo' })
+          }
+        }  else {
+          return res.status(400).json('no user to add new todo')
+        }
+      } else {
+        return res.status(400).json('no content or list_id in body')
+      }
+      
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
 })
 
 export default todoRouter
